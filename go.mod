79d77e0f33cde6555e6a7a92132734ae43ea947e
legendary-cookie <vincent.schweiger@xolley.de>
2022-03-04 00:24:59 +0100

    fancy argparse

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/akamensky/argparse v1.3.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
--- volume.go
+++ volume.go
@@ -5,6 +5,7 @@ import "fmt"
 import "github.com/jedib0t/go-pretty/v6/table"
 import "os"
 import "strconv"
+import "github.com/akamensky/argparse"
 
 func volumeHelp() {
 	fmt.Println("Available subcommands for <volume> are:\n- list\n- create <name> <size>\n- delete <name>\n- snapshot <volume> <snapshotname>\n- createpvc <volume> <namespace>")
@@ -16,22 +17,57 @@ func VolumeCommand(args []string) {
 	} else {
 		switch args[0] {
 		case "list":
-			volumes := GetVolumes().Data
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-			t.AppendHeader(table.Row{"Name", "Size", "Actual Size", "Ready", "State"})
-			for _, volume := range volumes {
-				size, err := strconv.ParseInt(volume.Size, 10, 64)
-				if err != nil {
-					log.Fatal(err)
-				}
-				actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
-				if err != nil {
-					log.Fatal(err)
-				}
-				t.AppendRow([]interface{}{volume.Name, ByteCountIEC(size), ByteCountIEC(actualSize), volume.Ready, volume.State})
+			parser := argparse.NewParser(os.Args[0]+" volume list", "")
+			o := parser.String("o", "output", &argparse.Options{Help: "Specify output format", Default: "table"})
+			err := parser.Parse(args)
+			if err != nil {
+				// In case of error print error and print usage
+				// This can also be done by passing -h or --help flags
+				fmt.Print(parser.Usage(err))
+				break
 			}
-			t.Render()
+			volumes := GetVolumes().Data
+			switch *o {
+			case "table":
+				t := table.NewWriter()
+				t.SetOutputMirror(os.Stdout)
+				t.AppendHeader(table.Row{"Name", "Size", "Actual Size", "Ready", "State"})
+				for _, volume := range volumes {
+					size, err := strconv.ParseInt(volume.Size, 10, 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+					actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+					t.AppendRow([]interface{}{volume.Name, ByteCountIEC(size), ByteCountIEC(actualSize), volume.Ready, volume.State})
+				}
+				t.Render()
+			case "csv":
+				for _, volume := range volumes {
+					size, err := strconv.ParseInt(volume.Size, 10, 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+					actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+					print(volume.Name)
+					print(",")
+					print(size)
+					print(",")
+					print(actualSize)
+					print(",")
+					print(volume.Ready)
+					print(",")
+					print(volume.State)
+					print("\n")
+				}
+			default:
+				fmt.Println("That output format doesn't exist!\nValid formats are: [table/json/csv]")
+			}
 		case "create":
 			if len(args) < 3 {
 				print(colorRed)
@@ -64,17 +100,17 @@ func VolumeCommand(args []string) {
 			volume := args[1]
 			name := args[2]
 			CreateSnapshot(volume, name)
-                case "createpvc":
-                      if len(args) < 3 {
-                                print(colorRed)
+		case "createpvc":
+			if len(args) < 3 {
+				print(colorRed)
 				fmt.Println("You have to specify the volume and the namespace!")
 				print(colorReset)
 				break
-                      }
-                      volume := args[1]
-                      ns := args[2]
-                      CreatePv(volume, volume)
-                      CreatePvc(volume, volume, ns)
+			}
+			volume := args[1]
+			ns := args[2]
+			CreatePv(volume, volume)
+			CreatePvc(volume, volume, ns)
 		default:
 			volumeHelp()
 		}
