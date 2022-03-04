fd15f32dafaf2d3ef3243b848bf24eb88aaf9e60
legendary-cookie <vincent.schweiger@xolley.de>
2022-03-04 15:49:17 +0100

    use argparse

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	_ "embed"
 	"errors"
+	"fmt"
 	"github.com/BurntSushi/toml"
+	"github.com/akamensky/argparse"
 	"log"
 	"os"
 	"os/user"
@@ -52,14 +54,48 @@ func HelpCommand() {
 }
 
 func Run(args []string) {
-	if len(args) != 0 {
-		switch args[0] {
-		case "volume":
-			VolumeCommand(args[1:])
-		default:
+	/*
+		if len(args) != 0 {
+			switch args[0] {
+			case "volume":
+				VolumeCommand(args[1:])
+			default:
+				HelpCommand()
+			}
+		} else {
 			HelpCommand()
 		}
+	*/
+	parser := argparse.NewParser("lhctl", "Longhorn CLI to manage different aspects of longhorn")
+	volumeCommand := parser.NewCommand("volume", "Manage and view volumes")
+	volumeListCommand := volumeCommand.NewCommand("list", "List all volumes")
+        listOutput := volumeListCommand.String("o", "output", &argparse.Options{Default: "table", Help: "Output format, valid are table, csv and json"})
+	volumeCreateCommand := volumeCommand.NewCommand("create", "Create a new volume")
+        volumeCreateName := volumeCreateCommand.String("n", "name", &argparse.Options{Required: true, Help: "The name of the volume to be created"})
+        volumeCreateSize := volumeCreateCommand.String("s", "size", &argparse.Options{Required: true, Help: "The size of the volume to be created"})
+	volumeDeleteCommand := volumeCommand.NewCommand("delete", "Delete a volume")
+        volumeDeleteName := volumeDeleteCommand.String("n", "name", &argparse.Options{Required: true, Help: "The name of the volume to be deleted"})
+	volumePvcCommand := volumeCommand.NewCommand("pvc", "Create a PV & PVC")
+        volumePvcName := volumePvcCommand.String("v", "volume", &argparse.Options{Required: true, Help:"The volume for which the PV and PVC should be created"})
+        volumePvcNamespace := volumePvcCommand.String("n", "namespace", &argparse.Options{Required: true, Help: "The namespace in which the PV and PVC should be created"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+	if volumeCommand.Happened() {
+		if volumeListCommand.Happened() {
+			ListVolumesCmd(*listOutput)
+		} else if volumeCreateCommand.Happened() {
+			VolumeCreateCmd(*volumeCreateName, *volumeCreateSize)
+		} else if volumeDeleteCommand.Happened() {
+			VolumeDeleteCmd(*volumeDeleteName)
+		} else if volumePvcCommand.Happened() {
+			VolumePvcCmd(*volumePvcName, *volumePvcNamespace)
+		}
 	} else {
-		HelpCommand()
+		// Should never get hit
+		err := fmt.Errorf("bad arguments, please check usage")
+		fmt.Print(parser.Usage(err))
 	}
 }
--- volume.go
+++ volume.go
@@ -5,114 +5,89 @@ import "fmt"
 import "github.com/jedib0t/go-pretty/v6/table"
 import "os"
 import "strconv"
-import "github.com/akamensky/argparse"
+import "encoding/json"
 
-func volumeHelp() {
-	fmt.Println("Available subcommands for <volume> are:\n- list\n- create <name> <size>\n- delete <name>\n- snapshot <volume> <snapshotname>\n- createpvc <volume> <namespace>")
+type VolumeListEntry struct {
+	Name       string `json:"name"`
+	Size       string `json:"size"`
+	ActualSize string `json:"actualSize"`
+	Ready      bool   `json:"ready"`
+	State      string `json:"state"`
 }
 
-func VolumeCommand(args []string) {
-	if len(args) == 0 {
-		volumeHelp()
-	} else {
-		switch args[0] {
-		case "list":
-			parser := argparse.NewParser(os.Args[0]+" volume list", "")
-			o := parser.String("o", "output", &argparse.Options{Help: "Specify output format", Default: "table"})
-			err := parser.Parse(args)
+
+func ListVolumesCmd(output string) {
+	volumes := GetVolumes().Data
+	switch output {
+	case "table":
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"Name", "Size", "Actual Size", "Ready", "State"})
+		for _, volume := range volumes {
+			size, err := strconv.ParseInt(volume.Size, 10, 64)
 			if err != nil {
-				// In case of error print error and print usage
-				// This can also be done by passing -h or --help flags
-				fmt.Print(parser.Usage(err))
-				break
+				log.Fatal(err)
 			}
-			volumes := GetVolumes().Data
-			switch *o {
-			case "table":
-				t := table.NewWriter()
-				t.SetOutputMirror(os.Stdout)
-				t.AppendHeader(table.Row{"Name", "Size", "Actual Size", "Ready", "State"})
-				for _, volume := range volumes {
-					size, err := strconv.ParseInt(volume.Size, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					t.AppendRow([]interface{}{volume.Name, ByteCountIEC(size), ByteCountIEC(actualSize), volume.Ready, volume.State})
-				}
-				t.Render()
-			case "csv":
-				for _, volume := range volumes {
-					size, err := strconv.ParseInt(volume.Size, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
-					if err != nil {
-						log.Fatal(err)
-					}
-					print(volume.Name)
-					print(",")
-					print(size)
-					print(",")
-					print(actualSize)
-					print(",")
-					print(volume.Ready)
-					print(",")
-					print(volume.State)
-					print("\n")
-				}
-			default:
-				fmt.Println("That output format doesn't exist!\nValid formats are: [table/json/csv]")
+			actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
+			if err != nil {
+				log.Fatal(err)
 			}
-		case "create":
-			if len(args) < 3 {
-				print(colorRed)
-				fmt.Println("You have to specify name and size!")
-				print(colorReset)
-				break
-			}
-			name := args[1]
-			size := args[2]
-			CreateVolume(name, size)
-		case "delete":
-			if len(args) < 2 {
-				print(colorRed)
-				fmt.Println("You have to specify the name of the volume you want to delete!")
-				print(colorReset)
-				break
-			}
-			name := args[1]
-			c := askForConfirmation("Do you really want to delete this volume?")
-			if c {
-				DeleteVolume(name)
-			}
-		case "snapshot":
-			if len(args) < 3 {
-				print(colorRed)
-				fmt.Println("You have to specify the volume and the name of the snapshot!")
-				print(colorReset)
-				break
-			}
-			volume := args[1]
-			name := args[2]
-			CreateSnapshot(volume, name)
-		case "createpvc":
-			if len(args) < 3 {
-				print(colorRed)
-				fmt.Println("You have to specify the volume and the namespace!")
-				print(colorReset)
-				break
-			}
-			volume := args[1]
-			ns := args[2]
-			CreatePv(volume, volume)
-			CreatePvc(volume, volume, ns)
-		default:
-			volumeHelp()
+			t.AppendRow([]interface{}{volume.Name, ByteCountIEC(size), ByteCountIEC(actualSize), volume.Ready, volume.State})
 		}
+		t.Render()
+	case "csv":
+		for _, volume := range volumes {
+			size, err := strconv.ParseInt(volume.Size, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			actualSize, err := strconv.ParseInt(volume.Controllers[0].ActualSize, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			print(volume.Name)
+			print(",")
+			print(size)
+			print(",")
+			print(actualSize)
+			print(",")
+			print(volume.Ready)
+			print(",")
+			print(volume.State)
+			print("\n")
+		}
+        case "json":
+          var entries []VolumeListEntry
+          for _, volume := range volumes {
+                        entry := VolumeListEntry{
+                          Name: volume.Name,
+                          Size: volume.Size,
+                          ActualSize: volume.Controllers[0].ActualSize,
+                          Ready: volume.Ready,
+                          State: volume.State,
+                        }
+                        entries = append(entries, entry)
+          }
+          jsonThing, err := json.Marshal(entries)
+          if err != nil {
+            log.Fatal(err)
+          }
+          println(string(jsonThing))
+	default:
+		fmt.Println("That output format doesn't exist!\nValid formats are: [table/json/csv]")
 	}
+}
+func VolumeCreateCmd(name, size string) {
+	CreateVolume(name, size)
+}
+
+func VolumeDeleteCmd(name string) {
+	c := askForConfirmation("Do you really want to delete this volume?")
+	if c {
+		DeleteVolume(name)
+	}
+}
+func VolumePvcCmd(volume, ns string) {
+	CreatePv(volume, volume)
+	CreatePvc(volume, volume, ns)
 }
